1eef43d3bad579f8a9c1801bddda87947ee477be
elgolondrino <apogeedecielo@gmail.com>
2015-05-09 20:24:09 +0500

    Works perfect

--- main.cpp
+++ main.cpp
@@ -1,28 +1,17 @@
 /*
- * ----------------------------------------------------------------------------
- * This is a MFRC522 library example; see https://github.com/miguelbalboa/rfid
- * for further details and other examples.
- *
- * NOTE: The library file MFRC522.h has a lot of useful info. Please read it.
- *
- * Released into the public domain.
- * ----------------------------------------------------------------------------
- * Example sketch/program which will try the most used default keys listed in
- * https://code.google.com/p/mfcuk/wiki/MifareClassicDefaultKeys to dump the
- * block 0 of a MIFARE RFID card using a RFID-RC522 reader.
- *
- * Typical pin layout used:
- * -----------------------------------------------------------------------------------------
- *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
- *             Reader/PCD   Uno           Mega      Nano v3    Leonardo/Micro   Pro Micro
- * Signal      Pin          Pin           Pin       Pin        Pin              Pin
- * -----------------------------------------------------------------------------------------
- * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
- * SPI SS      SDA(SS)      10            53        D10        10               10
- * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
- * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
- * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
- *
+ * ------------------------------------------------*
+ * Typical pin or GPIO layout used:                |
+ * ------------------------------------------------*
+ *             MFRC522      Raspberry Pi B+ &      |
+ *             Reader/PCD   2 B                    |
+ * Signal      Pin          Pin           GPIO     |
+ * ------------------------------------------------*
+ * RST/Reset   RST          22            25       |
+ * SPI SS      SDA(SS)      24            8        |
+ * SPI MOSI    MOSI         19            10       |
+ * SPI MISO    MISO         21            9        |
+ * SPI SCK     SCK          23            11       |
+ * ------------------------------------------------*
  */
 
 #include <bcm2835.h>
